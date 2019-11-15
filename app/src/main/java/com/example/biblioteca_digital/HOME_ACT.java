752fde813cab8982ac3898e195962c22946b07fc
ShadowDragonxX <56663152+ShadowDragonxX@users.noreply.github.com>
2019-11-15 11:00:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/biblioteca_digital/HOME_ACT.java
@@ -0,0 +1,14 @@
+package com.example.biblioteca_digital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HOME_ACT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__act);
+    }
+}
